23d76362d59b71dc3c462814c2725be274d70618
Filip Tammergård <filip@tammergard.se>
2022-05-24 13:28:25 +0200

    chore: initial commit

---
+++ astro.config.mjs
@@ -0,0 +1,10 @@
+import { defineConfig } from 'astro/config';
+import preact from '@astrojs/preact';
+
+import react from "@astrojs/react";
+import sitemap from "@astrojs/sitemap";
+
+// https://astro.build/config
+export default defineConfig({
+  integrations: [preact(), react(), sitemap()]
+});
